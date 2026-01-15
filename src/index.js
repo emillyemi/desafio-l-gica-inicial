615153e97c14a8013ef4ae9eb6bf9b6a27bc6aba
emillyemi <emillystofel010@gmail.com>
2026-01-15 11:23:05 -0300

    calculadora de rankeada para jogos

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
+let vitorias = 100
+let derrotas = 2
 
-let nomeHeroi = "Eudora"
-let heroiXp = 7.600
-let nivel = " "
-
-if (heroiXp < 1.000) {
-    nivel = "Ferro"
-}
-else if (heroiXp <= 2.000) {
-    nivel = "Bronze"
-}
-else if (heroiXp <= 5.000) {
-    nivel = "Prata"
-}
-else if (heroiXp <= 7.000) {
-     nivel = "Ouro"
-}
-else if (heroiXp <= 8.000) {
-     nivel = "Platina"
-}
-else if (heroiXp <= 9.000) {
- nivel = "Ascendente"
-}
-else if (heroiXp <= 10.000) {
-  nivel = "Imortal"
-}
-else
-    { (heroiXp >= 10.001) 
-  nivel = "Radiante"
+score (vitorias, derrotas)
+function score(vitorias, derrotas){
+ nivel = " "
+ saldoVitorias = vitorias - derrotas
+      if (saldoVitorias < 10){
+          nivel = "Ferro"
+          }
+        else if(saldoVitorias <= 20){
+            nivel = "Bronze"
+            }
+         else if (saldoVitorias <= 50){
+            nivel = "Prata"
+            }
+            else if(saldoVitorias <= 80){
+               nivel = "Ouro"
+               }
+                 else if(saldoVitorias <= 90){
+            nivel = "Diamante"
+            }
+            else if (saldoVitorias <= 100){
+          		nivel = "Lendário"
+                }
+           else if(saldoVitorias >= 101){
+               nivel = "Imortal"
+               }
 }
 
-console.log(" A heroína de nome " + nomeHeroi + " está no nível de " + nivel )
+
+	console.log("O Herói tem saldo de " + (saldoVitorias) + " e está no nível de " + (nivel))
